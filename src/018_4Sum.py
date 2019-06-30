30b7371c5d07327d6a198bc31118bcf0fb75a3b3
BrianQcq <qiuchuanqin@gmail.com>
2019-06-29 17:52:19 -0700

    1

--- src/018_4Sum.py
+++ src/018_4Sum.py
@@ -14,6 +14,7 @@ A solution set is:
   [-2,  0, 0, 2]
 ]
 """
+
 class Solution(object):
 	def fourSum(self, nums, target):
 		nums.sort()
@@ -46,7 +47,7 @@ class Solution(object):
 			for i in range(0, len(nums)-N+1):
 				if target < nums[i]*N or target > nums[-1]*N:
 					break
-				if i == 0 or i > 0 and nums[i-1] != nums[i]:
+				if i == 0 or (i > 0 and nums[i-1] != nums[i]):
 					self.findNSum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
 		return
 
@@ -56,3 +57,36 @@ res=A.fourSum([-1,0,-5,-2,-2,-4,0,1,-2], -9)
 res2=A.fourSum([0,0,0,0],0)
 print(res)
 print(res2)
+
+class Solution2(object):
+	def fourSum(self, nums, target):
+		def findNSum(l, r, target, N, result, results):
+			if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:
+				return
+
+			if N == 2:
+				while l < r:
+					s = nums[l] + nums[r]
+					if s == target:
+						results.append(result + [nums[l], nums[r]])
+						l += 1
+						while l < r and nums[l] == nums[l-1]:
+							l += 1
+					elif s < target:
+						l += 1
+					else:
+						r -= 1
+			else:
+				for i in range(l, r + 1):
+					if i==l or (i > l and nums[i-1] != nums[i]):
+						findNSum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
+
+		nums.sort()
+		results = []
+		findNSum(0, len(nums)-1, target, 4, [], results)
+		return results
+
+
+B=Solution2()
+Bres = B.fourSum([-1,0,-5,-2,-2,-4,0,1,-2], -9)
+print(Bres)
