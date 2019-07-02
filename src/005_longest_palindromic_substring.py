234489af3d6c0a709fef924e189d6da86e10c597
BrianQcq <qiuchuanqin@gmail.com>
2019-07-01 21:49:20 -0700

    1

--- src/005_longest_palindromic_substring.py
+++ src/005_longest_palindromic_substring.py
@@ -12,15 +12,33 @@ Output: "bb"
 
 class Solution(object):
 	def longestPalindrome(self, s):
-		pass
+		res = ""
+		for i in range(len(s)):
+			temp = self.helper(s,i,i)
+			if len(temp) > len(res):
+				res = temp
+
+			temp = self.helper(s,i,i+1)
+			if len(temp) > len(res):
+				res = temp
+		return res
 
 	def helper(self, s, l, r):
 		while l >= 0 and r < len(s) and s[l] == s[r]:
 			l -= 1; r += 1
 		return s[l+1:r]
 
+# DP solution
 
+class Solution2(object):
+	def longestPalindrome(self, s):
+		if len(s) == 0:
+			return 0
+		maxLen = 1
+		start = 0
+		for i in range(len(s)):
+			pass
 
 A=Solution()
-res=A.helper("abaac", 3, 3)
+res=A.helper("babac", 1, 1)
 print(res)
