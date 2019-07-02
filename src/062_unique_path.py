f8f8c9a2c09f1418a3cb590fb312790d29fb321c
BrianQcq <qiuchuanqin@gmail.com>
2019-07-02 16:24:17 -0700

    1

---
+++ src/062_unique_path.py
@@ -0,0 +1,45 @@
+"""
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Input: m = 7, n = 3
+Output: 28
+"""
+
+class Solution(object):
+
+	# recursion, time limit expires
+
+	def uniquePaths(self, m, n):
+		if m == 1 or n == 1:
+			return 1
+		return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+
+
+	# DP algo
+
+	def uniquePaths_2(self, m, n):
+		table = [[1 for x in range(n)] for x in range(m)]
+
+		for i in range(1, m):
+			for j in range(1, n):
+				table[i][j] = table[i-1][j] + table[i][j-1]
+		return table[m-1][n-1]
+
+
+
+A=Solution()
+res=A.uniquePaths_2(7,3)
+print(res)
