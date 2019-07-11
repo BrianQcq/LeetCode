f5ef1021d5bd195bf4031c948462757cde7434dc
BrianQcq <qiuchuanqin@gmail.com>
2019-07-10 18:41:03 -0700

    1

---
+++ src/21_merge_two_sorted_list.py
@@ -0,0 +1,18 @@
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+
+class Solution(object):
+	def merge(self, l1, l2):
+		if not l1:
+			return l2
+		if not l2:
+			return l1
+
+		if l1.val <= l2.val:
+			l1.next = self.merge(l1.next, l2)
+			return l1
+		else:
+			l2.next = self.merge(l1, l2.next)
+			return l2
