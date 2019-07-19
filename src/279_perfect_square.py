c4872a1691d87f67be08f8b3e9f8bcc8bcffc746
BrianQcq <qiuchuanqin@gmail.com>
2019-07-19 15:03:19 -0700

    1

---
+++ src/279_perfect_square.py
@@ -0,0 +1,46 @@
+
+class Solution(object):
+
+	 # BFS solution
+	def numSquares(object, n):
+		if n <= 3:
+			return n
+		lst = []
+		i = 1
+		while i * i <= n:
+			lst.append(i*i)
+			i += 1
+		#print(lst)
+		cnt = 0
+		toCheck = {n}
+		while toCheck:
+			cnt += 1
+			temp = set()
+			for x in toCheck:
+				for y in lst:
+					if x == y:
+						return cnt
+					if x < y:
+						break
+					temp.add(x-y)
+			toCheck = temp
+		return cnt
+
+	# DP solution
+	def numSquares_2(self, n):
+		# if n <= 3:
+		# 	return n
+		dp = [2**31] * (n+1)
+		dp[0] = 0
+		for i in range(1, len(dp)):
+			j = 1
+			while j*j <= i:
+				dp[i] = min(dp[i], dp[i-j*j] + 1)
+				j += 1
+		return dp[-1]
+
+
+
+A=Solution()
+res=A.numSquares_2(18)
+print(res)
