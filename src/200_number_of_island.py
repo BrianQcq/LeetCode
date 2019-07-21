777ee7c16f919145508cda767092499966f029c4
BrianQcq <qiuchuanqin@gmail.com>
2019-07-21 16:45:07 -0700

    1

--- src/200_number_of_island.py
+++ src/200_number_of_island.py
@@ -1,8 +1,10 @@
 
+# DFS solution
+
 class Solution(object):
 
 	def numIsland(self, grid):
-		if not grid:
+		if not grid or not grid[0]:
 			return 0
 		count = 0
 		for i in range(len(grid)):
@@ -25,3 +27,51 @@ class Solution(object):
 A=Solution()
 res=A.numIsland([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]])
 print(res)
+
+
+# BFS solution
+from collections import deque	
+
+class Solution_BFS(object):
+
+	# def numIsland(self, grid):
+	# 	if not grid or not grid[0]:
+	# 		return 0
+	# 	count = 0
+	# 	for i in range(len(grid)):
+	# 		for j in range(len(grid[0])):
+	# 			if grid[i][j] == '1':
+	# 				self.bfs(grid, i, j)
+	# 				count += 1
+	# 	return count
+
+	# def bfs(self, grid, i, j):
+	# 	pass
+
+	def numIslands(self, grid):
+		if not grid or not grid[0]:
+			return 0
+		r, c = len(grid), len(grid[0])
+		count = 0
+		for i in range(r):
+			for j in range(c):
+				if grid[i][j] == '1':
+					grid[i][j] = '0'
+					count += 1
+					queue = deque([(i-1,j),(i+1,j),(i,j-1),(i,j+1)])
+					while queue:
+						#print(queue)
+						x,y = queue.popleft()
+						if x < 0 or x >= r or y < 0 or y >= c:
+							continue
+						else:
+							if grid[x][y] == '0':
+								continue
+							else:
+								grid[x][y] = '0'
+								queue.extend([(x-1,y),(x+1,y),(x,y-1),(x,y+1)])
+		return count
+
+B=Solution_BFS()
+res_B = B.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]])
+print(res_B)
