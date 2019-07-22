0711e083b8ae685bfa112d4c6e555f9b648972e9
BrianQcq <qiuchuanqin@gmail.com>
2019-07-21 18:36:08 -0700

    1

---
+++ src/133_clone_graph.py
@@ -0,0 +1,12 @@
+"""
+# Defination for a node
+class Node(object):
+	def __init__(self, val, neighbors):
+		self.val = val
+		self.neighbors = neighbors
+"""
+
+
+class Solution(object):
+	def cloneGraph(self, node):
+		pass
